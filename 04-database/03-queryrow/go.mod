c9d4071972eecefcd1a8e827a20acc780616876c
bruno gonzaga santos <brunog86@gmail.com>
2024-02-01 23:38:08 -0300

    banco de dados

---
+++ 04-database/03-queryrow/go.mod
@@ -0,0 +1,8 @@
+module github.com/obrunogonzaga/pos-go-expert/04-database/01
+
+go 1.22rc1
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/google/uuid v1.6.0
+)
---
+++ 04-database/04-multiples-rows/main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
+
+type Product struct {
+	ID    string
+	Name  string
+	Price float64
+}
+
+func NewProduct(name string, price float64) *Product {
+	return &Product{
+		ID:    uuid.New().String(),
+		Name:  name,
+		Price: price,
+	}
+
+}
+
+func main() {
+	// Create a new product
+	product := NewProduct("Shoes", 100.00)
+
+	// Save the product to the database
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = insertProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+
+	// Update the product
+	product.Price = 350.00
+	err = updateProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+
+	// Select the product
+	p, err := selectProduct(db, product.ID)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Product %v, possuit o preco de %.2f\n", p.Name, p.Price)
+
+	// Select all products
+	products, err := selectProducts(db)
+	if err != nil {
+		panic(err)
+	}
+	for _, p := range products {
+		fmt.Printf("Product %v, possuit o preco de %.2f\n", p.Name, p.Price)
+	}
+}
+
+func selectProducts(db *sql.DB) ([]Product, error) {
+	rows, err := db.Query("SELECT id, name, price FROM products")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var products []Product
+	for rows.Next() {
+		var p Product
+		err := rows.Scan(&p.ID, &p.Name, &p.Price)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+	}
+	return products, nil
+}
+
+func selectProduct(db *sql.DB, id string) (*Product, error) {
+	stmt, err := db.Prepare("SELECT id, name, price FROM products WHERE id = ?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	var p Product
+	err = stmt.QueryRow(id).Scan(&p.ID, &p.Name, &p.Price)
+	//err = stmt.QueryRowWithContext(ctx, id).Scan(&p.ID, &p.Name, &p.Price) // It`s possible to use context, check time out, and etc.
+	if err != nil {
+		return nil, err
+	}
+	return &p, nil
+}
+
+func insertProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("INSERT INTO products (id, name, price) VALUES (?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func updateProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("UPDATE products SET name = ?, price = ? WHERE id = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.Name, product.Price, product.ID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
