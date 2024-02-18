8ce068927609991e01b8895363da9e48a11183f9
Bruno Gonzaga Santos <brunog86@gmail.com>
2024-02-18 12:41:02 -0300

    feat: create user endpoint

--- 07-APIS/cmd/server/main.go
+++ 07-APIS/cmd/server/main.go
@@ -29,6 +29,9 @@ func main() {
 	productDB := database.NewProduct(db)
 	productHandler := handlers.NewProductHandler(productDB)
 
+	userDB := database.NewUser(db)
+	userHandler := handlers.NewUserHandler(userDB)
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Post("/products", productHandler.CreateProduct)
@@ -36,6 +39,8 @@ func main() {
 	r.Get("/products", productHandler.GetProducts)
 	r.Put("/products/{id}", productHandler.UpdateProduct)
 	r.Delete("/products/{id}", productHandler.DeleteProduct)
+
+	r.Post("/users", userHandler.Create)
 
 	http.ListenAndServe(":8000", r)
 }
---
+++ 07-APIS/internal/infra/webserver/handlers/user_handlers.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"encoding/json"
+	"github.com/obrunogonzaga/pos-go-expert/07-APIS/07-APIS/internal/dto"
+	"github.com/obrunogonzaga/pos-go-expert/07-APIS/07-APIS/internal/entity"
+	"github.com/obrunogonzaga/pos-go-expert/07-APIS/07-APIS/internal/infra/database"
+	"net/http"
+)
+
+type UserHandler struct {
+	UserDB database.UserInterface
+}
+
+func NewUserHandler(userDB database.UserInterface) *UserHandler {
+	return &UserHandler{UserDB: userDB}
+}
+
+func (h *UserHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var user dto.CreateUserInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	u, err := entity.NewUser(user.Name, user.Email, user.Password)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = h.UserDB.Create(u)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
