d09ffe09bf0ae8aec4f4bf121ca203e31d10aee2
Bruno Gonzaga Santos <brunog86@gmail.com>
2024-02-18 10:31:18 -0300

    feat: create handlers and endpoint

--- 07-APIS/cmd/server/main.go
+++ 07-APIS/cmd/server/main.go
@@ -3,6 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/obrunogonzaga/pos-go-expert/07-APIS/07-APIS/configs"
+	"github.com/obrunogonzaga/pos-go-expert/07-APIS/07-APIS/internal/entity"
+	"github.com/obrunogonzaga/pos-go-expert/07-APIS/07-APIS/internal/infra/database"
+	"github.com/obrunogonzaga/pos-go-expert/07-APIS/07-APIS/internal/infra/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"net/http"
 )
 
 func main() {
@@ -11,4 +17,16 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(cfg.DBDriver)
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+	http.ListenAndServe(":8000", nil)
 }
