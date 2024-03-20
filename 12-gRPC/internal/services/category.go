32ceab43a48fa2300b6f5f849f91bbf39c2883aa
Bruno Gonzaga Santos <brunog86@gmail.com>
2024-03-19 23:11:18 -0300

    feat:  Get Categories using gRPC

--- 12-gRPC/internal/services/category.go
+++ 12-gRPC/internal/services/category.go
@@ -31,3 +31,36 @@ func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCateg
 
 	return &pb.CategoryResponse{Category: categoryResponse}, nil
 }
+
+func (c *CategoryService) ListCategories(ctx context.Context, in *pb.Blank) (*pb.CategoryList, error) {
+	categories, err := c.CategoryDB.FindAll()
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Error listing categories: %v", err)
+	}
+
+	var categoryList []*pb.Category
+	for _, category := range categories {
+		categoryList = append(categoryList, &pb.Category{
+			Id:          category.ID,
+			Name:        category.Name,
+			Description: category.Description,
+		})
+	}
+
+	return &pb.CategoryList{Categories: categoryList}, nil
+}
+
+func (c *CategoryService) GetCategory(ctx context.Context, in *pb.CategoryGetRequest) (*pb.CategoryResponse, error) {
+	category, err := c.CategoryDB.Find(in.GetId())
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Error getting category: %v", err)
+	}
+
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+
+	return &pb.CategoryResponse{Category: categoryResponse}, nil
+}
